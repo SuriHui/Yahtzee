f9d8f7499df2401687a527a969796cedaf203343
SuriHui <143148869+SuriHui@users.noreply.github.com>
2023-12-12 16:22:13 -0800

    Update Yahtzee.java
    work on roll dienumber and everything underneath

--- Yahtzee/Yahtzee.java
+++ Yahtzee/Yahtzee.java
@@ -1,32 +1,24 @@
 public class Yahtzee
 {
-    Die6 die1;
-    Die6 die2;
-    Die6 die3;
-    Die6 die4;
-    Die6 die5;
+    Die6[] dice = new Die6[5];
     /**
      * Constructor for objects of class Dice
      */
     public Yahtzee()
     {
-        die1 = new Die6();
-        die2 = new Die6();
-        die3 = new Die6();
-        die4 = new Die6(); 
-        die5 = new Die6();
+        for (int i = 0; i < dice.length; i++) {
+            dice[i] = new Die6;
+        }
     }
     
     public void roll() {
-        die1.roll();
-        die2.roll();
-        die3.roll();
-        die4.roll();
-        die5.roll();
+        for (int i = 0; i < dice.length; i++) {
+            dice[i].roll();
+        }
     }
     
     public int getValue() {
-        return die1.getValue()+die2.getValue()+die3.getValue()+die4.getValue()+die5.getValue();
+        return dice[0].getValue()+dice[1].getValue()+dice[2].getValue()+dice[3].getValue()+dice[4].getValue();
     }
     
     public int rollAndGetValue() {
@@ -35,9 +27,7 @@ public class Yahtzee
     }
     
     public void roll(int dieNumber) {
-        int count = 0;
-        count++;
-        for (count = 0; count <= 3; count++) {
+        for (int count = 0; count <= 3; count++) {
             if (dieNumber == 1) {
                 die1.roll();
             } else if (dieNumber == 2) {
