6cc3b2ac07ed6d7fdb92260cca3da9211cfaa406
Suri Hui <suri.hui@mrjaffesclass.com>
2023-12-13 11:39:49 -0800

    summarize and to dtring

---
+++ Scope test/ScopePractice.java
@@ -0,0 +1,8 @@
+public static void almostDone() {
+  int i = 0;
+  for (j = 0; j < 10; j++) {
+    System.out.println("My favorite number is still " + j);
+  }
+  System.out.println("I guess I still have lots of favorite     
+    numbers!");
+}
