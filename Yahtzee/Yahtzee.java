ebaf34d0ccc9a37c5c651b90e3b4cea759197305
Suri Hui <suri.hui@mrjaffesclass.com>
2023-12-14 11:19:27 -0800

    fix whatever and check?

--- Yahtzee/Yahtzee.java
+++ Yahtzee/Yahtzee.java
@@ -7,7 +7,7 @@ public class Yahtzee
     public Yahtzee()
     {
         for (int i = 0; i < dice.length; i++) {
-            dice[i] = new Die6;
+            dice[i] = new Die6();
         }
     }
     
@@ -29,30 +29,45 @@ public class Yahtzee
     public void roll(int dieNumber) {
         for (int count = 0; count <= 3; count++) {
             if (dieNumber == 1) {
-                die1.roll();
+                dice[0].roll();
             } else if (dieNumber == 2) {
-                die2.roll();
+                dice[1].roll();
             } else if (dieNumber == 3) {
-                die3.roll();
+                dice[2].roll();
             } else if (dieNumber == 4) {
-                die4.roll();
+                dice[3].roll();
             } else if (dieNumber == 5) {
-                die5.roll();
+                dice[4].roll();
             } else if (count > 3) {
                 System.out.print("YOU CANNOT REROLL MORE THAN 3 TIMES!!!");
             }
         }
     }
     
-    public void summarize() {
-        String[] strDie = {"1 - ","2 - ","3 - ","4 - ","5 - "};
-        for (int value = 0; value < strDie.length; value++) {
-            System.out.println(strDie[value]);
+    public String summarize() {
+        int ones = 0;
+        int twos = 0;
+        int threes = 0;
+        int fours = 0;
+        int fives = 0;
+        for (int i = 0; i < dice.length - 1; i++) {
+           if (dice[i].getValue == 1) {
+               ones++;
+           } else if (dice[i].getValue == 2) {
+               twos++;
+           } else if (dice[i].getValue == 3) {
+               threes++;
+           } else if (dice[i].getValue == 4) {
+               fours++;
+           } else if (dice[i].getValue == 5) {
+               fives++;
+           }
         }
+        return "1 - " + ones + "2 - " + twos + "3 - " + threes + "4 - " + fours + "5 - " + fives;
     }
     
     public String toString() {
-        String dieStr = "Dice Values: " + die1.getValue() + " " + die2.getValue() + " " + die3.getValue() + " " + die4.getValue() + " " + die5.getValue();  
+        String dieStr = "Dice Values: " + dice[0].getValue() + " " + dice[1].getValue() + " " + dice[2].getValue() + " " + dice[3].getValue() + " " + dice[4].getValue();  
         return dieStr;
     }
 }
